b4f7d92ed6d6bc00d3604258d9656143639cb9ce
rockop11 <rockoponce1422@gmail.com>
2025-12-04 11:12:57 -0300

    se creo el seed de prisma, se agrego variable de entorno de produccion

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,8 +5,9 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: 'tsx src/lib/prisma/seed.ts'
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env("POSTGRES_PRISMA_URL"),
   },
 });
