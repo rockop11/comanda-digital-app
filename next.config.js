4010e969732c6451a3d523ff08cdeef6b9ca6f38
rockop11 <rockoponce1422@gmail.com>
2026-01-26 16:50:57 -0300

    se modifico next config por problemas de build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+// Injected content via Sentry wizard
+import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,13 +15,8 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
-
-// Injected content via Sentry wizard below
-
-import { withSentryConfig } from "@sentry/nextjs"
-
-module.exports = withSentryConfig(module.exports, {
+// Exportar con Sentry config
+export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
@@ -41,17 +38,10 @@ module.exports = withSentryConfig(module.exports, {
   // side errors will fail.
   tunnelRoute: "/monitoring",
 
-  webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
+  // Uncomment below to update webpack configuration for working with Cloudflare Pages and Sentry.
+  // transpilePackages: ['@sentry/nextjs'],
+  // hideSourceMaps: true,
 
-    // Tree-shaking options for reducing bundle size
-    treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      removeDebugLogging: true,
-    },
-  },
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
 });
