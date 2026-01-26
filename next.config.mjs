5d9f05ccbe6a09f72f82ec1b8bb23dfe4ca67dc6
rockop11 <rockoponce1422@gmail.com>
2026-01-26 19:25:48 -0300

    se modifico el next.config

---
+++ next.config.mjs
@@ -0,0 +1,37 @@
+// Injected content via Sentry wizard
+import { withSentryConfig } from "@sentry/nextjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+        pathname: '/restaurants/**',
+      },
+    ],
+  },
+  experimental: {
+    webpackBuildWorker: true
+  }
+};
+
+// Exportar con Sentry config
+export default withSentryConfig(nextConfig, {
+  org: "comanda-digital",
+  project: "comanda-digital-app",
+
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  telemetry: false,
+
+  webpack: {
+    treeshake: {
+      removeDebugLogging: true
+    }
+  }
+});
